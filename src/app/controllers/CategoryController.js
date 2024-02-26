da1f0b16bfd7744d5fe963c4f770e52a661ee9a8
Doritao <carloshenriqueneves6@gmail.com>
2024-02-26 18:54:08 -0300

    adicionando try catch

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -2,49 +2,46 @@ import * as Yup from "yup";
 import Category from "../models/Category";
 import User from "../models/User";
 class CategoryController {
-
-  
-
   async store(request, response) {
-    const schema = Yup.object().shape({
-      name: Yup.string().required(),
-    });
-
     try {
-      await schema.validate(request.body, { abortEarly: false });
-
-      const {admin: isAdmin} = await User.findByPk(request.userId)
-
-      
-
-      if(!isAdmin) {
-        return response.status(401).json()
-      }
-
-
-      const { name } = request.body;
-
-      const { filename: path } = request.file;
-
-
-      const categoryExists = await Category.findOne({
-        where: {
-          name
-        }
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
       });
 
-      if (categoryExists) {
-        return response.status(400).json({
-          error: "This category already exists"
+      try {
+        await schema.validate(request.body, { abortEarly: false });
+
+        const { admin: isAdmin } = await User.findByPk(request.userId);
+
+        if (!isAdmin) {
+          return response.status(401).json();
+        }
+
+        const { name } = request.body;
+
+        const { filename: path } = request.file;
+
+        const categoryExists = await Category.findOne({
+          where: {
+            name,
+          },
         });
+
+        if (categoryExists) {
+          return response.status(400).json({
+            error: "This category already exists",
+          });
+        }
+
+        const { id } = await Category.create({ name, path });
+
+        return response.json({ id, name });
+      } catch (err) {
+        console.log(err);
+        return response.status(400).json({ error: err.errors });
       }
-
-      const {id} = await Category.create({ name, path });
-
-      return response.json({id, name});
     } catch (err) {
       console.log(err);
-      return response.status(400).json({ error: err.errors });
     }
   }
 
@@ -52,9 +49,6 @@ class CategoryController {
     const categories = await Category.findAll();
     return response.json(categories);
   }
-
-
-  
 }
 
 export default new CategoryController();
